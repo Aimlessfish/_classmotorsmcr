3258a8af4c0258995868f601b3bb2040a3f75db6
Aimlessfish <96752196+Aimlessfish@users.noreply.github.com>
2023-02-26 13:15:39 +0000

    Update proxy_scheduling.py
    fixed tabbing issues

--- active_bot/hpi_tets/proxy_scheduling.py
+++ active_bot/hpi_tets/proxy_scheduling.py
@@ -19,48 +19,50 @@ session = requests.Session()
 def testProxy():
 	now = datetime.datetime.now()
 	timestamp = now.strftime('%Y-%m-%d %H:%M:%S')
-    print(f"[{timestamp}] {info_statement} [Console]: Checking proxies.")
-    with open(r"C:\Users\Administrator\Desktop\_classmotorsmcr-main\required_list\proxy.txt","r") as f:
-        proxy_list = f.read().split('\n')
-    #await message.channel.send("Checking 10 proxies..")
-    proxies = proxy_list[0:10]
-    for proxy in proxies:
-        print(f"[{timestamp}] {info_statement} [Console]: proxy checked.")
-        try: 
-            response = session.get("http://ident.me/", proxies={'http': f"http://{proxy}"}, timeout=30)
-            if response.status_code == 200: 
-                proxy_list.pop(proxy_list.index(proxy))
-                with open(r"C:\Users\Administrator\Desktop\_classmotorsmcr-main\required_list\proxy.txt","w") as f:
-                    f.write("\n".join(proxy_list))
-                with open(r"C:\Users\Administrator\Desktop\_classmotorsmcr-main\required_list\working.txt","a") as f:
-                    f.write(f"{proxy} \n")
-            else:
-                proxy_list.pop(proxy_list.index(proxy))
-                with open(r"C:\Users\Administrator\Desktop\_classmotorsmcr-main\required_list\proxy.txt","w") as f:
-                    f.write("\n".join(proxy_list))
-        except Exception as e: 
-            proxy_list.pop(proxy_list.index(proxy))
-            with open(r"C:\Users\Administrator\Desktop\_classmotorsmcr-main\required_list\proxy.txt","w") as f:
-                f.write("\n".join(proxy_list))
-    print(f"[{timestamp}] {info_statement} [Console]: proxies checked.")
+	print(f"[{timestamp}] {info_statement} [Console]: Checking proxies.")
+	with open(r"C:\Users\Administrator\Desktop\_classmotorsmcr-main\required_list\proxy.txt","r") as f:
+		proxy_list = f.read().split('\n')
+#await message.channel.send("Checking 10 proxies..")
+	proxies = proxy_list[0:10]
+	for proxy in proxies:
+		print(f"[{timestamp}] {info_statement} [Console]: proxy checked.")
+		try: 
+			response = session.get("http://ident.me/", proxies={'http': f"http://{proxy}"}, timeout=30)
+			if response.status_code == 200: 
+				proxy_list.pop(proxy_list.index(proxy))
+				with open(r"C:\Users\Administrator\Desktop\_classmotorsmcr-main\required_list\proxy.txt","w") as f:
+					f.write("\n".join(proxy_list))
+				with open(r"C:\Users\Administrator\Desktop\_classmotorsmcr-main\required_list\working.txt","a") as f:
+					f.write(f"{proxy} \n")
+			else:
+				proxy_list.pop(proxy_list.index(proxy))
+				with open(r"C:\Users\Administrator\Desktop\_classmotorsmcr-main\required_list\proxy.txt","w") as f:
+					f.write("\n".join(proxy_list))
+		except Exception as e: 
+			proxy_list.pop(proxy_list.index(proxy))
+			with open(r"C:\Users\Administrator\Desktop\_classmotorsmcr-main\required_list\proxy.txt","w") as f:
+				f.write("\n".join(proxy_list))
+			print(f"[{timestamp}] {info_statement} [Console]: proxies checked.")
 
 def get_newProxy():
 	now = datetime.datetime.now()
 	timestamp = now.strftime('%Y-%m-%d %H:%M:%S')
-    print(f"[{timestamp}] {info_statement} [Console]: Clearing `working.txt`")
-    with open(r"C:\Users\Administrator\Desktop\_classmotorsmcr-main\required_list\working.txt","a") as f:
-        f.truncate(0)
-        f.close()
-    driver = webdriver.Chrome()
-    driver.get("http://list.didsoft.com/get?email=sales@classmotorsmcr.co.uk&pass=Soontoberich1&pid=http3000&showcountry=no&https=yes&excludedcountry=CN|RU")
-    proxyList = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"body > pre:nth-child(1)")))
-    proxies = proxyList.text
-    with open(r"C:\Users\Administrator\Desktop\_classmotorsmcr-main\required_list\proxy.txt","a") as f:
-        f.write("\n"+proxies)
-    print(f"[{timestamp}] {info_statement} [Console]: new proxies added.")
+	print(f"[{timestamp}] {info_statement} [Console]: Clearing `working.txt`")
+	with open(r"C:\Users\Administrator\Desktop\_classmotorsmcr-main\required_list\working.txt","a") as f:
+		f.truncate(0)
+		f.close()
+	driver = webdriver.Chrome()
+	driver.get("http://list.didsoft.com/get?email=sales@classmotorsmcr.co.uk&pass=Soontoberich1&pid=http3000&showcountry=no&https=yes&excludedcountry=CN|RU")
+	proxyList = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"body > pre:nth-child(1)")))
+	proxies = proxyList.text
+	with open(r"C:\Users\Administrator\Desktop\_classmotorsmcr-main\required_list\proxy.txt","a") as f:
+		f.write("\n"+proxies)
+	print(f"[{timestamp}] {info_statement} [Console]: new proxies added.")
 
 async def run_schedule():
-    print(f"[{timestamp}] {info_statement} [Console]: Starting schedule!")
+	now = datetime.datetime.now()
+	timestamp = now.strftime('%Y-%m-%d %H:%M:%S')
+	print(f"[{timestamp}] {info_statement} [Console]: Starting schedule!")
 
 	schedule.every().day.at("06:00").do(get_newProxy)
 	schedule.every().day.at("06:02").do(testProxy)
@@ -155,7 +157,7 @@ async def run_schedule():
 
 
 
-	
+
 	while True:
 	    schedule.run_pending()
 	    await asyncio.sleep(1)
