be9c03289966917a01c08cbd92e8ac386c0cb433
Aimlessfish <96752196+Aimlessfish@users.noreply.github.com>
2023-03-03 15:50:10 +0000

    Update reg_function.py

--- active_bot/reg_function.py
+++ active_bot/reg_function.py
@@ -264,8 +264,8 @@ async def reg(message, registration, miles):
 			#await message.channel.send("Failed to enter phone number")
 			logging.error(e, exc_info=True)
 		await message.channel.send("Getting report...")
-		driver_options.remove_argument("--proxy-server=http://"+proxy)
-		driver_options.remove_argument("--user-agent="+user_agent)
+		driver_options.pop_argument("--proxy-server=http://"+proxy)
+		driver_options.pop_argument("--user-agent="+user_agent)
 		try:
 			WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH,"//button[@class='btn btn-primary onboarding__btn onboarding__btn--next']"))).click();
 			await asyncio.sleep(2)
