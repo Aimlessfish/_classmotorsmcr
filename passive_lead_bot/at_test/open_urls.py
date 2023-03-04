3a875e17e79d0c6dc4f1bddfbd2c182f39136df8
Aimlessfish <96752196+Aimlessfish@users.noreply.github.com>
2023-03-04 16:52:49 +0000

    Update open_urls.py

--- passive_lead_bot/at_test/open_urls.py
+++ passive_lead_bot/at_test/open_urls.py
@@ -109,5 +109,23 @@ async def main():
     asyncio.create_task(start())
 
 
+@client.event
+async def on_ready():
+	now = datetime.datetime.now()
+	timestamp = now.strftime('%Y-%m-%d %H:%M:%S')
+	print(f'[{timestamp}] {info_statement} Logged in as [{client.user}]')
+	guilds = client.guilds
+	print(f'[{timestamp}] {info_statement} Connected to {len(guilds)} guild(s):')
+	for guild in guilds:
+		print(f'[{timestamp}] {info_statement} - {guild.name} (ID: {guild.id})')
+		for channel in guild.channels:
+			if channel.name == 'leads':
+				print(f'[{timestamp}] {info_statement} - Found leads-channel in {guild.name} (ID: {guild.id}), channel ID: {channel.id}')
+				global leads_channel
+				leads_channel = client.get_channel(channel.id)
+	# asyncio.create_task(run_schedule())
+	asyncio.create_task(start())
+
 if __name__ == '__main__':
-    asyncio.run(main())
+    clientID = 'MTA2ODgzODc4OTM3MzUwOTY3Mg.GFApDS.0zYDIu4XqbBVsLrhwyK3WB2wok0gAVjA-Su85w'
+    client.run(clientID)
