af4212a909b985b68fc879e3ebe4da68c368e260
Aimlessfish <96752196+Aimlessfish@users.noreply.github.com>
2023-03-13 22:25:01 +0000

    Update new.py

--- passive_lead_bot/at_test/new.py
+++ passive_lead_bot/at_test/new.py
@@ -28,6 +28,8 @@ info_statement = "[INFO    ]"
 logging.basicConfig(filename='lead_bot_errors.log', level=logging.ERROR)
 #-----------global settings
 
+max_retry = 5
+
 class RandomManager:
 	def __init__(self):
 		self.namesfile = r"C:\Users\Administrator\Desktop\_classmotorsmcr-main\required_list\names.txt"
@@ -36,6 +38,9 @@ class RandomManager:
 		self.names = []
 		self.postcodes = []
 		self.addresses = []
+		self.load_names()
+		self.load_postcodes()
+		self.load_addresses()
 
 	def load_names(self):
 		with open(self.namesfile, "r") as f:
@@ -125,16 +130,43 @@ class ProxyManager:
 	    else:
 	        print(f"{proxy} not found in proxy list.")
 
+class FileManager():
+	def __init__(self):
+		self.urlfile = r'C:\Users\Administrator\Desktop\_classmotorsmcr-main\required_list\urls.txt'
+		self.validfile = r'C:\Users\Administrator\Desktop\_classmotorsmcr-main\required_list\valid_url.txt'
+		self.urls = []
+		self.validurls = []
+		self.loadurl()
+		self.loadvalid()
+
+	def loadurl(self):
+		with open(self.urlfile, "+") as f:
+			self.urls = [line.strip() for line in f]
+		return self.urls
+
+	def loadvalid(self):
+		with open(self.validfile, "+") as f:
+			self.validurls = [line.strip() for line in f]
+		return self.validurls
+
+	def write_url(self, url):
+		with open(self.urlfile, "+") as f:
+			f.write(url+"\n")
+
+	def write_valid(self, valid_url):
+		with open(self.validfile, "+") as f:
+			f.write(valid_url+"\n")
+
 async def scrapeAT():
-	randommanage = RandomManager()
-	proxymanage = ProxyManager()
+	randommanager = RandomManager()
+	proxymanager = ProxyManager()
+	filemanager = FileManager()
 	driver_options = webdriver.ChromeOptions()
 	i = 0
-	max_retry = 3
 	while i < max_retry:
-		proxy = proxymanage.get_random_proxy()
+		proxy = proxymanager.get_random_proxy()
 		driver_options.add_argument("--proxy-server=http://"+proxy)
-		driver_options.add_argument("--user-agent="+proxymanage.get_random_UA())
+		driver_options.add_argument("--user-agent="+proxymanager.get_random_UA())
 		driver_options.add_argument("--start-maximized")
 		driver_options.add_argument("--disable-blink-features=AutomationControlled")
 		driver_options.add_experimental_option("excludeSwitches", ["enable-automation"])
@@ -203,36 +235,99 @@ async def scrapeAT():
 		#await message.channel.send("failed to refine search")
 	pageCount=5
 	i=0
+	filemanager = FileManager()
 	while i < pageCount:
 		html = driver.page_source
 		soup = BeautifulSoup(html, 'html.parser')
 
-		with open(r'C:\Users\Administrator\Desktop\_classmotorsmcr-main\required_list\urls.txt', 'r') as file:
-			existing_urls = file.readlines()
+		existing_urls = filemanager.urls
 
-		with open(r'C:\Users\Administrator\Desktop\_classmotorsmcr-main\required_list\urls.txt', 'a') as file:
-			for article in soup.find_all('article', class_='product-card js-standout-listing'):
-				a_tag = article.find('a', class_='js-click-handler listing-fpa-link tracking-standard-link')
-				if a_tag:
-					url = a_tag['href']
-					if 'http://autotrader.co.uk' + url + '\n' not in existing_urls:
-						file.write('http://autotrader.co.uk' + url + '\n')
+		for article in soup.find_all('article', class_='product-card js-standout-listing'):
+			a_tag = article.find('a', class_='js-click-handler listing-fpa-link tracking-standard-link')
+			if a_tag:
+				url = a_tag['href']
+				if 'http://autotrader.co.uk' + url + '\n' not in existing_urls:
+					filemanager.write_url('http://autotrader.co.uk' + url)
+
 		try:
 			next_page_button = WebDriverWait(driver,2).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"a[class='paginationMini--right__active'] i[class='icon']")))
 			if next_page_button:
 				next_page_button.click()
-				i = i+1
-				await asyncio.sleep(2)
+				i += 1
+			await asyncio.sleep(2)
 		except TimeoutException as e:
 			logging.error(e, exc_info=True)
-			i = i+1
+			i += 1
 			#await message.channel.send("Couldn't get next page of listings")
 	driver.quit()
+
+async def checkFinance():
+	randommanager = RandomManager()
+	proxymanager = ProxyManager()
+	filemanager = FileManager()
+	now = datetime.datetime.now()
+	timestamp = now.strftime('%Y-%m-%d %H:%M:%S')
+	print(f"{timestamp} {info_statement} [Console]: Proxy connection failed: retrying.")
+	randommanage = RandomManager()
+	proxymanage = ProxyManager()
+	driver_options = webdriver.ChromeOptions()
+	proxy = proxymanager.get_random_proxy()
+	i = 0
+	url_pool = filemanager.urls
+	for url in url_pool:
+		while i != max_retry:
+			driver_options.add_argument("--proxy-server=http://"+proxy)
+			driver_options.add_argument("--user-agent="+proxymanager.get_random_UA())
+			driver_options.add_argument("--start-maximized")
+			driver = webdriver.Chrome(options = driver_options)
+			# Changing the property of the navigator value for webdriver to undefined 
+			driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})") 
+			try:
+				driver.get(url)
+				if "Auto Trader UK" in driver.title:
+					print("Connected to AutoTrader..")
+					break  # exit loop if page loaded successfully
+			except Exception as e:
+				logging.error(e, exc_info=True)
+				i += 1
+				now = datetime.datetime.now()
+				timestamp = now.strftime('%Y-%m-%d %H:%M:%S')
+				print(f"{timestamp} {info_statement} [Console]: Proxy connection failed: retrying. {retry_counter}")
+				proxymanage.remove_proxy()
+				await asyncio.sleep(2)
+		try: #get price_text
+			global listing_price
+			find_price = WebDriverWait(driver,5).until(EC.presence_of_element_located((By.XPATH,"/html[1]/body[1]/div[2]/main[1]/div[1]/div[2]/aside[1]/section[1]/div[1]/div[1]/h2[1]")))
+			if find_price:
+				listing_price = find_price.text
+		except InvalidArgumentException as e: #end get price
+			logging.error(e, exc_info=True)
+		i = 0
+		while i < 1:
+			try: 
+				WebDriverWait(driver, 5).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"/html/body/div[4]/iframe")))
+				WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH,"/html/body/div/div[2]/div[3]/div[2]/button[2]"))).click();
+				i += 1
+			except TimeoutException as e:
+				i += 1
+				#await message.channel.send("Time-out finding cookies element.")
+				logging.error(e, exc_info=True)
+				break
+		driver.execute_script("window.scrollTo(0, 800)")
+		try:
+			finance_option = WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH,"/html/body/div[2]/main/div/div[2]/aside/section[3]/div/div/section/div/div[2]/div/button")))
+			if finance_option:
+				filemanager.write_valid(driver.current_url+"\n")
+
+		except TimeoutException as e:
+			logging.error(e,exc_info=True)
+			driver.quit()
 
 async def start(leads_channel):
 	await scrapeAT()
 	print("finished......")
 	await asyncio.sleep(10)
+	await checkFinance()
 
 @client.event
 async def on_ready():
