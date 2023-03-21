7a73da52d489abac20a68b869534b24caf447f81
Aimlessfish <96752196+Aimlessfish@users.noreply.github.com>
2023-03-21 13:33:26 +0000

    Update testnew_class.py

--- main210323/testnew_class.py
+++ main210323/testnew_class.py
@@ -10,9 +10,14 @@ from selenium.webdriver.common.by import By
 proxy_manager = ProxyManager()
 proxy = proxy_manager.get_random_proxy()
 
-driver = FirefoxDriver(proxy=proxy).get_driver()
-driver.get("https://whatsmyip.com")
+fox_driver = FirefoxDriver(proxy=proxy).get_driver()
+fox_driver.get("https://whatsmyip.com")
 time.sleep(5)
-raw_ipv4 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="shownIpv4"]')))
+raw_ipv4 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'/html/body/section/div/div/div/div[2]/p[1]')))
+ipv4 = raw_ipv4.text
+print(ipv4)
+chrome_driver = ChromeDriver.chromeDriver()
+chrome_driver.get("https://whatsmyip.com")
+raw_ipv4 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'/html/body/section/div/div/div/div[2]/p[1]')))
 ipv4 = raw_ipv4.text
 print(ipv4)
