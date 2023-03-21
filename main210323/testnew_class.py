6e14cb9b03eadada2ba2a8693d12571d5ae4b646
Aimlessfish <96752196+Aimlessfish@users.noreply.github.com>
2023-03-21 13:35:01 +0000

    Update testnew_class.py

--- main210323/testnew_class.py
+++ main210323/testnew_class.py
@@ -13,11 +13,11 @@ proxy = proxy_manager.get_random_proxy()
 fox_driver = FirefoxDriver(proxy=proxy).get_driver()
 fox_driver.get("https://whatsmyip.com")
 time.sleep(5)
-raw_ipv4 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'/html/body/section/div/div/div/div[2]/p[1]')))
+raw_ipv4 = WebDriverWait(fox_driver, 10).until(EC.element_to_be_clickable((By.XPATH,'/html/body/section/div/div/div/div[2]/p[1]')))
 ipv4 = raw_ipv4.text
 print(ipv4)
 chrome_driver = ChromeDriver.chromeDriver()
 chrome_driver.get("https://whatsmyip.com")
-raw_ipv4 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'/html/body/section/div/div/div/div[2]/p[1]')))
+raw_ipv4 = WebDriverWait(chrome_driver, 10).until(EC.element_to_be_clickable((By.XPATH,'/html/body/section/div/div/div/div[2]/p[1]')))
 ipv4 = raw_ipv4.text
 print(ipv4)
