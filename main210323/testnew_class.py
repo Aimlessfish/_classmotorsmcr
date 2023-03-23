df090f059d17555ab11389ad55b9da279e26e5bb
Aimlessfish <96752196+Aimlessfish@users.noreply.github.com>
2023-03-23 00:06:44 +0000

    Update testnew_class.py

--- main210323/testnew_class.py
+++ main210323/testnew_class.py
@@ -9,9 +9,17 @@ from selenium.webdriver.common.by import By
 
 proxy_manager = ProxyManager()
 proxy = proxy_manager.get_random_proxy()
-
+maxretry = 5
+i = 0 
 fox_driver = FirefoxDriver(proxy=proxy).get_driver()
-fox_driver.get("https://whatsmyip.com")
+while i != maxretry:
+	proxy = proxy_manager.get_random_proxy()
+	fox_driver = FirefoxDriver(proxy=proxy).get_driver()
+	fox_driver.get("https://whatsmyip.com")
+	if "Whats My" in fox_driver.title:
+		break
+	else:
+		proxy_manager.remove_proxy(proxy)
 time.sleep(5)
 raw_ipv4 = WebDriverWait(fox_driver, 10).until(EC.element_to_be_clickable((By.XPATH,'/html/body/section/div/div/div/div[2]/p[1]')))
 ipv4 = raw_ipv4.text
