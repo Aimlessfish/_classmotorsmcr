0bd926b573b5dfca07d64790581786c951515b35
Aimlessfish <96752196+Aimlessfish@users.noreply.github.com>
2023-03-26 23:11:14 +0100

    added new main added try except to testnew_

---
+++ main210323/main.py
@@ -0,0 +1,35 @@
+import time
+import discord
+import requests
+import re 
+import logging
+import random
+import asyncio
+import schedule
+import datetime
+from classManager import ProxyManager
+from classManager import FirefoxDriver
+from classManager import ChromeDriver
+from classManager import RandomManager
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import InvalidArgumentException
+
+#-----------discord settings
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+#-----------discord settings
+
+#-----------global settings
+info_statement = "[INFO    ]"
+logging.basicConfig(filename='lead_bot_errors.log', level=logging.ERROR)
+#-----------global settings
+
+max_retry = 5
