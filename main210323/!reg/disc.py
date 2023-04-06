3c599d0f5265f9d3f387173b8ee1b40fcdfa6a99
Aimlessfish <96752196+Aimlessfish@users.noreply.github.com>
2023-04-06 14:51:56 +0100

    updates to proxyManager

--- main210323/!reg/disc.py
+++ main210323/!reg/disc.py
@@ -44,6 +44,7 @@ async def on_message(message):
             randomProxy = proxyManager.get_random_proxy()
             proxy = proxyManager.testProxy(randomProxy)
             while not proxy:
+                proxyManager.remove_proxy(randomProxy)
                 randomProxy = proxyManager.get_random_proxy()
                 proxy = proxyManager.testProxy(randomProxy)
             await message.channel.send("Proxy Selection Completed!")
