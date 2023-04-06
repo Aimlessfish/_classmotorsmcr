2d8ae0cef5ae4b24760c2485d0852b92786d2b70
Aimlessfish <96752196+Aimlessfish@users.noreply.github.com>
2023-04-06 14:35:14 +0100

    Update disc.py

--- main210323/!reg/disc.py
+++ main210323/!reg/disc.py
@@ -37,7 +37,7 @@ async def on_message(message):
 
     if isinstance(message.channel, discord.DMChannel):
         if message.content.startswith('!reg'):
-            message.channel.send("Proxy Selection Started.")
+            await message.channel.send("Proxy Selection Started.")
             drivers = [ChromeDriver, FirefoxDriver]
             selected_driver = random.choice(drivers)
             useragent = proxyManager.get_random_UA()
@@ -46,7 +46,7 @@ async def on_message(message):
             while not testproxy:
                 randomProxy = proxyManager.get_random_proxy()
                 proxy = proxyManager.testProxy(randomProxy)
-            message.channel.send("Proxy Selection Completed!")
+            await message.channel.send("Proxy Selection Completed!")
             driverInstance = selected_driver.create(proxy, useragent)
             driver_info = f"[SELECTED DRIVER    {driverInstance}]"
             print(f"{info} {driver_info}")
