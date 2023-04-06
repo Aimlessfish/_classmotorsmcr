1ce09d422440ef32455e10277ce46ea63c0e65fe
Aimlessfish <96752196+Aimlessfish@users.noreply.github.com>
2023-04-06 15:58:35 +0100

    Create testIP.py

---
+++ main210323/!reg/testIP.py
@@ -0,0 +1,25 @@
+from classManager import FirefoxDriver
+from classManager import ChromeDriver
+from classManager import ProxyManager
+import random
+import requests
+import time
+
+proxyManager = ProxyManager()
+
+drivers = [ChromeDriver, FirefoxDriver]
+selected_driver = random.choice(drivers)
+useragent = proxyManager.get_random_UA()
+randomProxy = proxyManager.get_random_proxy()
+proxy = proxyManager.testProxy(randomProxy)
+while not proxy:
+    proxyManager.remove_proxy(randomProxy)
+    randomProxy = proxyManager.get_random_proxy()
+    proxy = proxyManager.testProxy(randomProxy)
+
+driverInstance = selected_driver.create(proxy=proxy, useragent=useragent)
+driver_info = f"[SELECTED DRIVER    {driverInstance}]"
+activeDriver = driverInstance.get_driver()
+
+activeDriver.get("http://httpbin.org/ip")
+time.sleep(10)
